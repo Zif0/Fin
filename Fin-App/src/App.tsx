434b4dee8d5b7fcf0a518e13b78e9dc05359984f
Zi <zfeng2@wgu.edu>
2025-05-16 22:47:22 -0700

    Minor Update 1 - App color scheme, slight nav changes

--- Fin-App/src/App.tsx
+++ Fin-App/src/App.tsx
@@ -3,10 +3,13 @@ import Navigation from "./Navigation";
 function App() {
   return (
     <>
-    <div>
+    <div className="Welcome">
       <Navigation />
-      <h1>Welcome to Fin</h1>
-      <p>The one stop, financial application.</p>
+      <h1>Welcome to <b>Fin.</b></h1>
+      <p>The one stop, financial solution.</p>
+    </div>
+    <div className="WelcomeStart">
+      <p>Tap to Begin</p>
     </div>
     </>
   );
