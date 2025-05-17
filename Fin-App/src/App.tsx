ba6470a5b6d1c56cf71d7c87e14214dd9b68be49
Zi <zfeng2@wgu.edu>
2025-05-16 21:31:04 -0700

    Initial Build

---
+++ Fin-App/src/App.tsx
@@ -0,0 +1,15 @@
+import Navigation from "./Navigation";
+
+function App() {
+  return (
+    <>
+    <div>
+      <Navigation />
+      <h1>Welcome to Fin</h1>
+      <p>The one stop, financial application.</p>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Fin-App/src/Navigation.tsx
@@ -0,0 +1,23 @@
+function Navigation() {
+  return (
+    <>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    </>
+  );
+}
+
+export default Navigation;
